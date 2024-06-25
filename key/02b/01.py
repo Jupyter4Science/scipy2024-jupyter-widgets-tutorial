6333652e24d409a1d0f52056a7424377fe16edec
Juan Cabanela <juan.cabanela@go.mnstate.edu>
2024-06-25 13:20:51 -0500

    Essentially addressed almost all suggestions by @mwcraig on Jun 25.

--- key/02b/01.py
+++ key/02b/01.py
@@ -1,9 +1,11 @@
 
 def on_window_size_change(change):
     global original_df, selected_df # Remember to access the global variables
-    # catch the change in the window_size widget value and update the original_df
+    # Update the following call to set the original_df['Smoothed Data'] column
+    # values to properly capture the change in window_size value and the
+    # current poly_order value.
     original_df['Smoothed Data'] = savgol_filter(original_df['Temperature'],
                                                  change['new'],
-                                                 poly_order.value).round(decimals=3) # original_df['Smoothed Data'] = savgol_filter(original_df['Temperature'], window_size, poly_order).round(decimals=3)
+                                                 poly_order.value).round(decimals=3)
     selected_df = original_df[(original_df['Year'] >= year_range.value[0])
                               & (original_df['Year'] <= year_range.value[1])]
