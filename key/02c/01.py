b1af6071eea830035cf8f727396751d2eb7ae04b
Juan Cabanela <juan.cabanela@go.mnstate.edu>
2024-06-25 14:21:34 -0500

    CLeaned up 02c_layout.ipynb and updated the solutions.

---
+++ key/02c/01.py
@@ -0,0 +1,3 @@
+import ipywidgets as widgets
+from dashboard.widgets import (year_range, window_size, poly_order, selected_data_grid,
+                               update_selected_datagrid, plot_view, output_plot) # import year_range, window_size, poly_order, selected_data_grid, update_selected_datagrid, plot_view, output_plot
